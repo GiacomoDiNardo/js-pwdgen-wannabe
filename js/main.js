7f71ba6f054689d4befa7fec5e4138a378e3c753
GiacomoDiNardo <vest_95@hotmail.it>
2022-05-17 15:18:13 +0200

    create repo add js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const userName = prompt('Inserisci il tuo nome');
+const userSurname = prompt('iserisci il tuo cognome');
+const userColor = prompt('Qual è il tuo colore preferito?');
+
+const spanPassword = document.getElementById('user-password');
+spanPassword.innerHTML = `<strong>La tua password è:</strong> ${userName}${userSurname}${userColor}22`;
