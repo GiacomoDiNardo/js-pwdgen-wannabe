df734a113d4ee6850859b9f3c58d6206f20a1241
GiacomoDiNardo <vest_95@hotmail.it>
2022-05-17 16:25:06 +0200

    add a variable

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const userName = prompt('Inserisci il tuo nome');
 const userSurname = prompt('iserisci il tuo cognome');
 const userColor = prompt('Qual è il tuo colore preferito?');
+const year = "22"
 
 const spanPassword = document.getElementById('user-password');
-spanPassword.innerHTML = `<strong>La tua password è:</strong> ${userName}${userSurname}${userColor}22`;
+spanPassword.innerHTML = `<strong>La tua password è:</strong> ${userName}${userSurname}${userColor}${year}`;
